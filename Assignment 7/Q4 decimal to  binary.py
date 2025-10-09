29157def9215f99f09be97e8638071b1da246de7
21cegan <21cegan.acc@lwetb.ie>
2025-10-09 09:08:12 +0100

    Update Q4 decimal to  binary.py
    added some notes

--- Assignment 7/Q4 decimal to  binary.py
+++ Assignment 7/Q4 decimal to  binary.py
@@ -9,13 +9,14 @@ decimal = int(decimal)
 while decimal != 0:
     remainder = decimal % 2
     
-    binary.insert(0,int(remainder))
+    binary.insert(0,int(remainder)) #adds the result to the first place in the list
     
     if remainder == 1:
         decimal -= 1
     decimal /= 2
 binarystr = ""
 for x in binary:
-    binarystr += str(x)
+    binarystr += str(x) #turns the list into a string so that it looks nicer
 print(binarystr)
+
 
