6c36baf0719fc1d4df1efccc44b8369b1204406f
21cegan <21cegan.acc@lwetb.ie>
2025-12-04 09:37:32 +0000

    Added comments

--- UTF encoder and decoder.py
+++ UTF encoder and decoder.py
@@ -12,7 +12,7 @@ if input("Enter 'e' to encode or anything else to decode: ") == "e":
     binary = ""
     for x in UTF:
         if x in hexa:
-            x = decimal[hexa.index(x)]
+            x = decimal[hexa.index(x)] #turns decimal numbers above 10 into hexadecimal
         digit = ""
         remainder = 0
         x = int(x)
@@ -21,20 +21,22 @@ if input("Enter 'e' to encode or anything else to decode: ") == "e":
             remainder = x % 2
             digit = str(remainder) + digit
             x //= 2
+            
         while len(digit) < 4:
-            digit = "0" + digit
+            digit = "0" + digit #add 0s to the digit to reach 4 binary digits per hexadecimal digits
         binary += digit
         
-    binary = binary[binary.find("1"):]
-    if len(binary) <= 7:
+    binary = binary[binary.find("1"):] #cut the excess 0s off the start of the binary number
+    
+    if len(binary) <= 7: #check if binary can fit into the first row
         encoded = "0" + binary
         while len(encoded) !=8:
             encoded = "0" + encoded
             
-    elif len(binary) <= 11:
-        encoded = binary[:-6] + " 10" + binary[-6:] 
+    elif len(binary) <= 11: 
+        encoded = binary[:-6] + " 10" + binary[-6:] #fits the binary digits into the row
         while len(encoded) !=14:
-            encoded = "0" + encoded
+            encoded = "0" + encoded #adds extra 0s to fill in missing space
         encoded = "110" + encoded
         
     elif len(binary) <= 16:
@@ -50,13 +52,15 @@ if input("Enter 'e' to encode or anything else to decode: ") == "e":
         encoded = "11110" + encoded
     
     print(encoded)
+
+#decoding
 else:
     
     encoded = input("Enter an encoded UTF value: ")
     binary = ""
     
-    if encoded[0] != 0:
-        while " " in encoded:
+    if encoded[0] != "0":
+        while " " in encoded: #remove the row indicators
             first0 = encoded.index("0")
             space = encoded.index(" ")
             binary += encoded[first0+1:space]
@@ -67,12 +71,12 @@ else:
     while binary:
         power = 0
         y = 0
-        for x in binary[:-5:-1]:
+        for x in binary[:-5:-1]: #gets the last 4 binary digits to convert to hexa
             y += int(x)*(2**power)
             power += 1
         binary = binary[:-4]
         if y in decimal:
-            UTF = hexa[decimal.index(y)] + UTF
+            UTF = hexa[decimal.index(y)] + UTF #
         else:
             UTF = str(y) + UTF
     print("U+"+UTF)
