adc409a863913f1a4dd21b18c014a788ab76eb70
21cegan <21cegan.acc@lwetb.ie>
2026-04-21 22:19:36 +0100

    Create OL Question16_B.py

---
+++ Exam Question/2023 OL/OL Question16_B.py
@@ -0,0 +1,9 @@
+# Question 16(b)
+# Name and School:Callum Egan
+
+flight_number= input("Enter your flight number")
+if flight_number[-1] == "2":
+    print("This is a direct flight")
+elif flight_number[-1] == "5":
+    print("This is an indirect flight")
+
