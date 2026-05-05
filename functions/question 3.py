61fddc88715cacff6a41c9994776a6fb984e75f6
21cegan <21cegan.acc@lwetb.ie>
2026-05-05 14:21:30 +0100

    Add files via upload

---
+++ functions/question 3.py
@@ -0,0 +1,12 @@
+"""
+Author:Callum Egan
+30/04/26
+Description: MAKES TWENTY: Given two integers, return True if the sum of the integers is 20 or if one
+of the integers is 20. If not, return False
+"""
+def makes_twenty(a,b):
+    if a == 20 or b == 20 or a+b == 20:
+        return True
+    else:
+        return False
+print(makes_twenty(10,20))
